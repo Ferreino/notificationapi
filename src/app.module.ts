2cbc70580360426093e534429ef612b2b0468744
Fernando Reino <fsreino@sudamericano.edu.ec>
2025-05-19 21:33:17 -0500

    semanaa8

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';  
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { NotificationsModule } from './notifications/notifications.module';
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '123456',
+  database: 'notification_db',
+  autoLoadEntities: true,
+  synchronize: true,
+  }),
+  NotificationsModule,
+
+
+  ],
+controllers: [AppController],
+providers: [AppService],
+
+})
+
+
+export class AppModule {}
+
+
+
---
+++ src/notifications/notifications.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationsService } from './notifications.service';
+import { NotificationsController } from './notifications.controller';
+import { Notification } from './notification.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Notification])],
+  controllers: [NotificationsController],
+  providers: [NotificationsService],
+  exports: [NotificationsService], // Exportamos el servicio para que pueda ser utilizado en otros módulos
+})
+export class NotificationsModule {}
