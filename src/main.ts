48db85a1297d84aa110c0289008b9637f8048c14
Fernando Reino <fsreino@sudamericano.edu.ec>
2025-06-13 19:35:22 -0500

    back12

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-, origin: process.env.CORS_ORIGIN || '*', // Cambia esto al origen de tu frontend
+    origin: process.env.FRONTEND_URL || '*', // Cambia esto al origen de tu frontend
   });
 
 const port = process.env.PORT || 3000;
